9a6434e3bbd55c859cdff08167a5921643820c90
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-04 18:09:11 +0530

    Time Convertor

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,19 @@ function calculateArea(){
     document.getElementById("output").innerHTML=
     `<p>Area is ${(len * wid)} sq.mtr.</p><p>Perimeter is ${(2 * (len + wid))} mtr</p>`;
 }
+
+function convertor(){
+    let h=parseInt(document.getElementById("len").value);
+   
+    var res=0;
+    var p;
+    if(document.getElementsByName('hours')[0].checked){
+        res=h*60;
+        p="mins";
+    }
+    if(document.getElementsByName('hours')[1].checked){
+        res=h*60*60;
+        p="seconds";
+    }
+    document.getElementById("output").innerHTML=`${h} hours = ${res} ${p}`;
+}
